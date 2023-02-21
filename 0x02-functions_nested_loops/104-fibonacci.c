90ba0974a8c91dc8b37a7686414be4c1ef3790d6
Joseph-Blessing <blessingfije28@gmail.com>
2023-02-21 09:12:18 -0800

    corrected header file

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,3 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the first 98 Fibonacci numbers, starting with
+ *        1 and 2, separated by a comma followed by a space.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 	int count;
@@ -41,3 +49,4 @@ int main(void)
 	printf("\n");
 	return (0);
 }
+
--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,4 +1,4 @@
-nclude "holberton.h"
+#include "main.h"
 
 /**
  * _islower - Checks if a character is lowercase.
